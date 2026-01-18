f7eb625647044cbc2f10efd8a9e2aadee99ecbf7
dahiya001rohit <rohit830770@gmail.com>
2026-01-18 15:24:42 +0530

    feat: Add AES-256 encryption and appToken JWT

--- server/controllers/JWT.js
+++ server/controllers/JWT.js
@@ -1,5 +1,6 @@
 const JWT = require('jsonwebtoken')
 const myJWTSecret = 'co-mailer-jwt-secert'
+const myAppPassJWTSecret = 'co-mailer-App-Pass-jwt-secert'
 function makeToken(user){
     const payload = {
         name: user.name,
@@ -14,7 +15,17 @@ function verifyToken(token){
     return JWT.verify(token, myJWTSecret)
 }
 
+function makeAppPassToken(appPass){
+    return JWT.sign({ appPass }, myAppPassJWTSecret, {
+        expiresIn: '4h'
+    })
+}
+function verifyAppToken(appToken){
+    return JWT.verify(appToken, myAppPassJWTSecret)
+}
 module.exports = {
     makeToken,
-    verifyToken
+    verifyToken,
+    makeAppPassToken,
+    verifyAppToken
 }
